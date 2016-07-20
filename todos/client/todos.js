242e3c91d561082457627ab05c1ff17cc7532df4
pgenev <philip_genev@abv.bg>
2016-07-21 00:50:52 +0300

    second_upload

--- todos/client/todos.js
+++ todos/client/todos.js
@@ -18,6 +18,26 @@ if(Meteor.isClient){
 		}
 	});
 
+	Template.todoItem.helpers({
+		'checked': function(){
+			var isCompleted = this.completed;
+			if(isCompleted){
+				return "checked";
+			} else {
+				return "";
+			}
+		}
+	});
+
+	Template.todosCount.helpers({
+		'totalTodos' : function() {
+			return Todos.find().count();
+		},
+		'completedTodos' : function() {
+			return Todos.find({completed: true}).count();
+		}
+	});
+
 	Template.todoItem.events({
 		'click .delete_todo' : function(event){
 			event.preventDefault();
@@ -28,14 +48,28 @@ if(Meteor.isClient){
 			}
 		},
 		'keyup [name=todoItem]': function(event){
-			    var documentId = this._id;
-				var todoItem = $(event.target).val();
-				Todos.update({ _id: documentId}, {$set: {name: todoItem}});
-				console.log("Task changed to: "+ todoItem);
+			    if(event.which == 13 || event.which == 27){
+			    	$(event.target).blur();
+			    }
+			    else{
+			    	var documentId = this._id;
+					var todoItem = $(event.target).val();
+					Todos.update({ _id: documentId}, {$set: {name: todoItem}});
+				}
 
+		},
+		'change [type=checkbox]' : function(){
+			var documentId = this._id;
+			var isCompleted = this.completed;
+			if(isCompleted){
+				Todos.update({ _id: documentId}, {$set: {completed: false}})
+				console.log("Task marked as incomplete.");
+			} else {
+				Todos.update({ _id: documentId}, {$set: {completed: true}})
+				console.log("Task marked as completed.");
+			}
 		}
-			
-		
 	});
+		
 
 }
